e220556539f98cb1811584c72585b54b38bff62e
Joel Varty <joelvarty@outlook.com>
2020-12-17 11:03:17 -0500

    id

--- components/agility-modules/RichTextArea.js
+++ components/agility-modules/RichTextArea.js
@@ -9,6 +9,7 @@ const RichTextArea =  (props) => {
 	return (
 		<Container>
 			<ContentWithPaddingLg>
+				<h3>{ props.properties.versionID }</h3>
 				<RichText className="prose" dangerouslySetInnerHTML={renderHTML(props.fields.textblob)}></RichText>
 			</ContentWithPaddingLg>
 		</Container>
